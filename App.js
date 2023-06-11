757aeec4b7739f59394d671882b5187e3dcdb095
looju <omofade2020@gmail.com>
2023-06-11 01:05:57 +0100

    renderer

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Expo from "expo";
+import { Scene, Mesh, MeshBasicMaterial, PerspectiveCamera } from "three";
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 
-export default function App() {
+const App = () => {
+  const onContextCreate = async () => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+  };
+
+  gl.canvas = { width: gl.drawingBufferWidth, height: gl.drawingBufferHeight };
+
+  const renderer = new Renderer({ gl });
+  renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <GLView onContextCreate={() => onContextCreate()} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
