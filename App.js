5efa5fa651f145777c5cc41ba67bc9068337880d
looju <omofade2020@gmail.com>
2023-06-11 01:13:55 +0100

    geometry

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Expo from "expo";
-import { Scene, Mesh, MeshBasicMaterial, PerspectiveCamera } from "three";
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxGeometry,
+} from "three";
 import ExpoTHREE, { Renderer } from "expo-three";
 import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 
@@ -23,10 +29,17 @@ const App = () => {
     const renderer = new Renderer({ gl });
     renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: "blue",
+    });
+
+    const cube = new Mesh(geometry, material);
+
     const render = () => {
       requestAnimationFrame(render);
       renderer.render(scene, camera);
-      gl.endFrameEXP()
+      gl.endFrameEXP();
     };
 
     render();
