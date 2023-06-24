e9cf403939a1b8fffa1fe29b2273e1d4d89d7209
looju <omofade2020@gmail.com>
2023-06-24 20:23:44 +0100

    use gl

--- App.js
+++ App.js
@@ -1,32 +1,73 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber/native";
-import { View } from "react-native";
-import { PerspectiveCamera, CameraControls,Environment,OrbitControls } from "@react-three/drei";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Expo from "expo";
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxGeometry,
+} from "three";
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 
-function CarShow() {
-  return (
-    <>
-      <Environment/>
-      <ambientLight intensity={0.5} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh rotation={[1, 3, 2]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
-    </>
-  );
-}
+const App = () => {
+  const onContextCreate = async (gl) => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
 
-function App() {
+    gl.canvas = {
+      width: gl.drawingBufferWidth,
+      height: gl.drawingBufferHeight,
+    };
+    camera.position.z = 5;
+
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: "blue",
+    });
+
+    const cube = new Mesh(geometry, material);
+
+    scene.add(cube);
+
+    const render = () => {
+      requestAnimationFrame(render);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+
+    render();
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-        <Canvas shadows>
-        <Suspense fallback={null}>
-          <CarShow />
-          </Suspense>
-        </Canvas>
+    <View>
+      <GLView onContextCreate={onContextCreate} style={styles.gl} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gl: {
+    width: 500,
+    height: 500,
+  },
+});
 
 export default App;
